26126376ad7656cc1b5f45a889c810cfdba5f49f
Le Duc Cuong <leduccuonghcmut@gmail.com>
2024-09-29 22:39:44 +0700

    Init for Exercise2

---
+++ STM32CubeIDE/Core/Inc/led_exerice2.h
@@ -0,0 +1,13 @@
+/*
+ * led_exerice2.h
+ *
+ *  Created on: Sep 29, 2024
+ *      Author: Admin
+ */
+#include "main.h"
+#ifndef INC_LED_EXERICE2_H_
+#define INC_LED_EXERICE2_H_
+
+void display7SEG(int num);
+
+#endif /* INC_LED_EXERICE2_H_ */
