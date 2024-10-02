8a10c8ce56c73a7a594a901fb2b8479a55bf1c00
Le Duc Cuong <leduccuonghcmut@gmail.com>
2024-10-02 15:20:27 +0700

    Update

--- STM32CubeIDE/Core/Src/main.c
+++ STM32CubeIDE/Core/Src/main.c
@@ -99,12 +99,6 @@ int main(void)
     setTimer(2, 5);
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-//    HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
-//    HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
-//    HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
-//    HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
-//    HAL_GPIO_WritePin(DOT_GPIO_Port, DOT_Pin, SET);
-//    display7SEG(-1);
   while (1)
   {
     /* USER CODE END WHILE */
